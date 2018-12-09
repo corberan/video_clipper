3bf335148148755d1860648c0f4c279df17ed0cf
CAi <8044101+alfnxd@users.noreply.github.com>
2018-12-09 19:02:31 +0800

    pims -> opencv

--- face_clustering.py
+++ face_clustering.py
@@ -2,6 +2,7 @@
 import api
 import models
 import os
+import platform
 import shutil
 from collections import OrderedDict
 
@@ -14,6 +15,36 @@ import click
 import pims
 import dlib
 from tqdm import tqdm
+import cv2
+
+
+# 在 Linux 下 pims 运行良好，内存占用仅 200 MB 左右，但在 Windows 上会不断分配内存直至无法分配最后报错
+# 可能是新版的 PyAV 导致的，所以暂时在 Windows 上切换为 opencv 读取视频
+# opencv 跳帧读取会消耗很大的 CPU 资源，没办法
+def open_video(video_file_path):
+    if platform.system() == "Windows":
+        vid = cv2.VideoCapture(video_file_path)
+        video_fps = vid.get(5)  # CV_CAP_PROP_FPS
+        video_frames_count = int(vid.get(7))  # CV_CAP_PROP_FRAME_COUNT
+    else:
+        vid = pims.Video(video_file_path)
+        video_fps = vid.frame_rate
+        video_frames_count = len(vid)
+
+    def get_frame(frame_num):
+        if frame_num > video_frames_count:
+            return None
+
+        if platform.system() == "Windows":
+            vid.set(1, frame_num)  # CV_CAP_PROP_POS_FRAMES
+            ret, frame = vid.read()
+            if not ret:
+                return None
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        else:
+            return vid[frame_num]
+
+    return video_fps, video_frames_count, get_frame
 
 
 def cluster(video_file_path, faces_output_path, skipped_duration, ending_time, step_duration, frame_scale_rate,
@@ -26,28 +57,30 @@ def cluster(video_file_path, faces_output_path, skipped_duration, ending_time, s
     sp = dlib.shape_predictor(models.pose_predictor_model_location())
     face_rec = dlib.face_recognition_model_v1(models.face_recognition_model_location())
 
-    # 使用 pims + pyav 来读取视频，不会将视频加载到内存中，避免出现处理体积较大的视频时内存不足的情况
-    vid = pims.Video(video_file_path)
+    video_fps, video_frames_count, get_frame = open_video(video_file_path)
 
     if skipped_duration is None or skipped_duration <= 0:
         skipped_frames_count = 0
     else:
-        skipped_frames_count = int(vid.frame_rate * skipped_duration)
+        skipped_frames_count = int(video_fps * skipped_duration)
 
     if ending_time is None or ending_time <= 0:
-        ending_frames_num = len(vid)
+        ending_frames_num = video_frames_count
     else:
-        ending_frames_num = min(int(vid.frame_rate * ending_time), len(vid))
+        ending_frames_num = min(int(video_fps * ending_time), video_frames_count)
 
     if step_duration is None or step_duration <= 0:
         step_frames_count = 1
     else:
-        step_frames_count = int(vid.frame_rate * step_duration)
+        step_frames_count = int(video_fps * step_duration)
 
     face_to_descriptor = OrderedDict()
 
     for frame_num in tqdm(range(skipped_frames_count, ending_frames_num, step_frames_count)):
-        frame = vid[frame_num]
+        frame = get_frame(frame_num)
+        if frame is None:
+            break
+
         if frame_scale_rate is not None:
             frame = dlib.resize_image(frame, scale=frame_scale_rate)
 
