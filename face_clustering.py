2168ec33123210a5bf765417add744b69d1eb3fb
CAi <8044101+alfnxd@users.noreply.github.com>
2018-12-10 23:06:21 +0800

    加入批量检测和识别

--- face_clustering.py
+++ face_clustering.py
@@ -2,9 +2,10 @@
 import api
 import models
 import os
-import platform
+from glob import glob
 import shutil
 from collections import OrderedDict
+from math import ceil
 
 try:
     import cPickle as pickle
@@ -12,43 +13,19 @@ except ImportError:
     import pickle
 
 import click
-import pims
 import dlib
 from tqdm import tqdm
-import cv2
 
 
-# 在 Linux 下 pims 运行良好，内存占用仅 200 MB 左右，但在 Windows 上会不断分配内存直至无法分配最后报错
-# 可能是新版的 PyAV 导致的，所以暂时在 Windows 上切换为 opencv 读取视频
-# opencv 跳帧读取会消耗很大的 CPU 资源，没办法
-def open_video(video_file_path):
-    if platform.system() == "Windows":
-        vid = cv2.VideoCapture(video_file_path)
-        video_fps = vid.get(5)  # CV_CAP_PROP_FPS
-        video_frames_count = int(vid.get(7))  # CV_CAP_PROP_FRAME_COUNT
-    else:
-        vid = pims.Video(video_file_path)
-        video_fps = vid.frame_rate
-        video_frames_count = len(vid)
-
-    def get_frame(frame_num):
-        if frame_num > video_frames_count:
-            return None
-
-        if platform.system() == "Windows":
-            vid.set(1, frame_num)  # CV_CAP_PROP_POS_FRAMES
-            ret, frame = vid.read()
-            if not ret:
-                return None
-            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        else:
-            return vid[frame_num]
-
-    return video_fps, video_frames_count, get_frame
+def get_frames(images_path_list, frame_scale_rate):
+    for f in images_path_list:
+        frame = dlib.load_rgb_image(f)
+        if frame_scale_rate is not None:
+            frame = dlib.resize_image(frame, scale=frame_scale_rate)
+        yield frame
 
 
-def cluster(video_file_path, faces_output_path, skipped_duration, ending_time, step_duration, frame_scale_rate,
-            gpu_memory_fraction, model):
+def cluster(faces_output_path, frame_scale_rate, iter_batch_size, gpu_memory_fraction, model):
     if model == 'mtcnn':
         face_detector = api.mtcnn_face_detector(gpu_memory_fraction)
     else:
@@ -57,82 +34,96 @@ def cluster(video_file_path, faces_output_path, skipped_duration, ending_time, s
     sp = dlib.shape_predictor(models.pose_predictor_model_location())
     face_rec = dlib.face_recognition_model_v1(models.face_recognition_model_location())
 
-    video_fps, video_frames_count, get_frame = open_video(video_file_path)
+    frame_face_descriptors = OrderedDict()
 
-    if skipped_duration is None or skipped_duration <= 0:
-        skipped_frames_count = 0
-    else:
-        skipped_frames_count = int(video_fps * skipped_duration)
+    image_file_suffix = '.jpg'
+    image_file_suffix_len = len(image_file_suffix)
+    images_path_list = [f for f in glob(os.path.join(faces_output_path, '?*{}'.format(image_file_suffix))) if
+                        os.path.isfile(f)]
+    images_size = len(images_path_list)
 
-    if ending_time is None or ending_time <= 0:
-        ending_frames_num = video_frames_count
-    else:
-        ending_frames_num = min(int(video_fps * ending_time), video_frames_count)
+    with tqdm(total=images_size) as pbar:
+        for i in range(ceil(images_size / iter_batch_size)):
+            beginning_index = i * iter_batch_size
+            ending_index = beginning_index + iter_batch_size
+            if ending_index > images_size:
+                images_path_batch = images_path_list[beginning_index:]
+            else:
+                images_path_batch = images_path_list[beginning_index:ending_index]
+            frames = get_frames(images_path_batch, frame_scale_rate)
+            for batch_index, (frame_dets, frame) in enumerate(face_detector(frames, 0)):
+                face_detections = dlib.full_object_detections()
+                frame_shape = frame.shape
+                for det in frame_dets:
+                    if type(det) == dlib.rectangle:
+                        face_detections.append(sp(frame, det))
+                    else:
+                        rect = det.rect
+                        new_left = max(rect.left() - round(rect.width() * 0.2), 0)
+                        # new_top = max(rect.top() - round(rect.height() * 0.2), 0)
+                        new_top = rect.top()
+                        new_right = min(rect.right() + round(rect.width() * 0.2), frame_shape[1])
+                        new_bottom = min(rect.bottom() + round(rect.height() * 0.3), frame_shape[0])
+                        face_detections.append(sp(frame, dlib.rectangle(new_left, new_top, new_right, new_bottom)))
 
-    if step_duration is None or step_duration <= 0:
-        step_frames_count = 1
-    else:
-        step_frames_count = int(video_fps * step_duration)
+                face_detections_size = len(face_detections)
+                frame_image_path = images_path_batch[batch_index]
 
-    face_to_descriptor = OrderedDict()
+                if face_detections_size > 0:
+                    frame_num = int(os.path.basename(frame_image_path)[:-image_file_suffix_len])
+                    face_descriptors = face_rec.compute_face_descriptor(frame, face_detections, 1)
+                    frame_face_descriptors[frame_num] = face_descriptors
 
-    for frame_num in tqdm(range(skipped_frames_count, ending_frames_num, step_frames_count)):
-        frame = get_frame(frame_num)
-        if frame is None:
-            break
+                    if face_detections_size == 1:
+                        chip_file_path = os.path.join(os.path.dirname(frame_image_path), '%d_1' % frame_num)
+                    else:
+                        chip_file_path = os.path.join(os.path.dirname(frame_image_path), '%d' % frame_num)
+                    dlib.save_face_chips(frame, face_detections, chip_file_path, size=150, padding=0)
 
-        if frame_scale_rate is not None:
-            frame = dlib.resize_image(frame, scale=frame_scale_rate)
+                os.remove(frame_image_path)
 
-        dets = face_detector(frame, 1)
-        for i, rect in enumerate(dets):
-            shape = sp(frame, rect)
-            face_descriptor = face_rec.compute_face_descriptor(frame, shape, 1)
-            #
-            face_id = '%d_%d' % (frame_num, i)
-            face_to_descriptor[face_id] = face_descriptor
-            #
-            file_path = os.path.join(faces_output_path, face_id)
-            dlib.save_face_chip(frame, shape, file_path, size=150, padding=0.25)
+            pbar.update(len(images_path_batch))
 
-    labels = dlib.chinese_whispers_clustering(list(face_to_descriptor.values()), 0.45)
+    face_descriptor_list = []
+    for face_descriptors in frame_face_descriptors.values():
+        for face_descriptor in face_descriptors:
+            face_descriptor_list.append(face_descriptor)
+
+    labels = dlib.chinese_whispers_clustering(face_descriptor_list, 0.45)
     class_labels = set(labels)
 
-    with open(os.path.join(faces_output_path, 'face_to_descriptor.pickle'), 'wb') as outfile:
-        pickle.dump(face_to_descriptor, outfile)
+    with open(os.path.join(faces_output_path, 'frame_face_descriptors.pickle'), 'wb') as outfile:
+        pickle.dump(frame_face_descriptors, outfile)
 
+    unknown_label_name = 'unknown'
     for class_label in class_labels:
-        label_folder_path = os.path.join(faces_output_path, str(class_label))
+        label_folder_path = os.path.join(faces_output_path, '%s-%s' % (str(class_label), unknown_label_name))
         if not os.path.isdir(label_folder_path):
             os.makedirs(label_folder_path)
 
-    for i, face_id in enumerate(face_to_descriptor.keys()):
-        filename = '%s.jpg' % face_id
-        file_path = os.path.join(faces_output_path, filename)
-        dst_path = os.path.join(faces_output_path, str(labels[i]), filename)
-        shutil.move(file_path, dst_path)
+    label_index = 0
+    for frame_num, face_descriptors in frame_face_descriptors.items():
+        for i in range(len(face_descriptors)):
+            filename = '%d_%d.jpg' % (frame_num, i + 1)
+            src_path = os.path.join(faces_output_path, filename)
+            dst_path = os.path.join(faces_output_path, '%s-%s' % (str(labels[label_index]), unknown_label_name),
+                                    filename)
+            shutil.move(src_path, dst_path)
+            label_index += 1
 
 
 @click.command()
-@click.argument('video_file_path')
 @click.argument('faces_output_path')
-@click.option('--skipped_duration', default=None, type=int, help='seconds')
-@click.option('--ending_time', default=None, type=int, help='seconds')
-@click.option('--step_duration', default=None, type=float, help='seconds')
-@click.option('--frame_scale_rate', default=None, type=float, help='for reducing memory usage')
+@click.option('--frame_scale_rate', default=None, type=float)
+@click.option('--iter_batch_size', default=1, type=int)
 @click.option('--gpu_memory_fraction', default=0.5, type=float)
 @click.option('--model', default=None, type=str)
-def main(video_file_path, faces_output_path, skipped_duration, ending_time, step_duration, frame_scale_rate,
-         gpu_memory_fraction, model):
-    if not os.path.isfile(video_file_path):
-        print('\"%s\" not found' % video_file_path)
-        return
+def main(faces_output_path, frame_scale_rate, iter_batch_size, gpu_memory_fraction, model):
     if not os.path.isdir(faces_output_path):
         print('\"%s\" not exists' % faces_output_path)
         return
 
-    cluster(video_file_path, faces_output_path, skipped_duration, ending_time, step_duration, frame_scale_rate,
-            gpu_memory_fraction, model)
+    cluster(faces_output_path, frame_scale_rate, iter_batch_size, gpu_memory_fraction, model)
 
 
 if __name__ == '__main__':
